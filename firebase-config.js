38d3dcd9178f319325f0f5b1d6c2d5ce7aa6ebdc
juansammi <4j7hskb9v9@privaterelay.appleid.com>
2026-07-11 16:47:50 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,14 @@
 export const DEFAULT_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDzh4etp331ytRyd3_2HQrl3pGTzKeUJ38",
+  apiKey: "AIzaSyDzh4etp33lytRyd3_2HQrl3pGTzKeUJ38",
   authDomain: "toeic-master-8e0f1.firebaseapp.com",
+  databaseURL: "https://toeic-master-8e0f1-default-rtdb.firebaseio.com",
   projectId: "toeic-master-8e0f1",
   storageBucket: "toeic-master-8e0f1.firebasestorage.app",
   messagingSenderId: "2478267193",
-  appId: "1:2478267193:web:2d0ef78dcc6b683a7826e8",
+  appId: "1:2478267193:web:2a0ef78dcc6b683a7826e8",
   measurementId: "G-MCW62M38QT"
 };
+
 export const ADMIN_EMAIL = "s111001@hcvs.hc.edu.tw";
 
 export function getFirebaseConfig() {
